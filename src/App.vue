e795b5b5c561b17184b97c5e61afffbe44a9732f
ruqayaahh <aderinolaruqayaah@gmail.com>
2021-03-19 19:28:35 +0100

    Reusable feature: sidebar design implementation

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,23 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+#sidebar-top-section {
+  height: 25%;
+  width: 100%;
+  background: #2B3C4E;
+  box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.25);
+  border: 1px solid #000000;
+  text-align: center;
 }
 </style>
---
+++ src/views/Sidebar.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <b-sidebar id="sidebar-no-header" v-model='visible' aria-labelledby="sidebar-no-header-title"
+    bg-variant='white' width='20%' no-header shadow class="border border-dark">
+        <div id="sidebar-top-section">
+            <b-img v-bind="mainProps" rounded="circle" alt="Circle image" src=""
+            class="mb-2"></b-img>
+            <div class="text-white">
+                <p>
+                   <span class="font-weight-bold">Jane doe</span><br>
+                <i>doe@enyata.com</i>
+                </p>
+            </div>
+        </div>
+        <div class="p-3 pl-0">
+          <nav class="m-3">
+            <b-nav vertical>
+              <b-nav-item active class="sidebar-menu">
+                  <img src="../assets/dashboard-icon.svg"
+                    class="d-inline-block mr-3">Dashboard</b-nav-item>
+              <b-nav-item to="" class="sidebar-menu">
+                  <img src="../assets/assessment-icon.svg"
+                    class="d-inline-block mr-3">Assesment</b-nav-item>
+              <b-nav-item to="" class="sidebar-menu mt-5">
+                  <img src="../assets/logout-icon.svg"
+                    class="d-inline-block mr-3">Log Out</b-nav-item>
+            </b-nav>
+          </nav>
+        </div>
+    </b-sidebar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar',
+  data() {
+    return {
+      visible: true,
+      mainProps: {
+        blank: true, blankColor: '#777', width: 100, height: 100, class: 'mt-4',
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+ .sidebar-menu {
+    font-size: 16px;
+    font-weight: 400;
+ }
+
+ a {
+    color: #2B3C4E;
+ }
+ a:hover {
+    color: #2B3C4E;
+    font-weight: 700;
+ }
+</style>
